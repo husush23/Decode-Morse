344c86f69c18baba830e026be109982016e632e3
husush23 <husseinkadare40@gmail.com>
2023-07-27 11:04:49 +0300

    Added the code

---
+++ decode.rb
@@ -0,0 +1,48 @@
+def decode_char(morse_code)
+morse_to_char = {
+  ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+  
+ return  morse_to_char[morse_code]
+end
+
+
+
+ def decode_word(morse_code)
+ morse_code.split.map{ |morse| decode_char(morse)}.join
+
+end
+
+def decode(morse_code)
+  words = morse_code.split("   ")
+  decoded_words = words.map{|word| decode_word(word) }
+  decoded_string = decoded_words.join(" ")
+
+end
+puts decode("-- -.--   -. .- -- .")
+
